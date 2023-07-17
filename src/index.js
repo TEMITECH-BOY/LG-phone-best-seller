eff4710a3c27303e431b54c3a7f55edb2b3107b6
TEMITECH-BOY <temitayoosunla@gmail.com>
2023-07-17 12:54:02 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  import {phones} from "./phones"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const img = "./images/phoneA.jpg";
+// const model = <h2>Boost Mobile LG stylo 6</h2>;
+// const price = <h2>$199.00</h2>;
+// const description = <h2>white- Prepaid Smartphone</h2>;
+// const ram = <h2>32GB</h2>;
+
+
+function PhoneList() {
+  return(
+    <>
+    <h1>LG-PHONE BEST-SELLERS</h1>
+  <section className="phonelist">
+    {phones.map((phone, index)=>{
+      return (<LgPhone {...phone} key={phone.id} number={index}/>)
+      })}
+  </section>
+  </>
+  );
+}
+
+const LgPhone = (props) => {
+  const {img,model,price,description,ram, number }= props;
+  console.log(props)
+  return (
+    <article className="phone">
+      <img src={img} alt={model}/>
+      <h2>{model}</h2>
+      <h2>{price}</h2>
+      <h4>{description}</h4>
+      <h4>{ram}</h4>
+      <span className="number">{number+1}</span>
+      
+    </article>
+  );
+};
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<PhoneList/>);
---
+++ src/phones.js
@@ -0,0 +1,38 @@
+ import img1 from "./images/phoneZ.webp";
+import img2 from "./images/phoneX.jpg"
+  import img3 from "./images/phoneY.webp"
+ import img4 from "./images/phoneZ.webp"
+ 
+ 
+  export const phones = [
+      {
+    img: img1,
+     model: "Boost Mobile LG  stylo 6",
+    price: "$199.00",
+    description: "White- Prepaid Smartphone",
+     ram: "32GB",
+    id: 1,
+  },
+  {     img: img2,
+     model: "LG K92 5G smartphone",
+    price: "$129.99",
+    description: "Internal memory 128GB- microSD slot-6.67",
+    ram: "6GB",
+     id: 2,
+   },
+  {
+    img: img3,
+    model: "AT&T LG Phoenix 5",
+    price: "$95.00",
+    description: "Silver- Prepaid Smartphone",
+    ram: "16GB",
+        id: 3,
+ },
+ {   img: img4,
+   model: "Boost Mobile LG K22",
+  price: "$69.00",
+   description: "Beige- Prepaid Smartphone",
+    ram: "32GB",
+   id: 4,
+ },
+];
